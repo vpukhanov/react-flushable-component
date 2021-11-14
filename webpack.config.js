0b8367f357c2f4eb489a2e487a5274c3f662fe8e
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2021-11-14 08:39:47 +0300

    add basic build configuration

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx'],
+  },
+}
